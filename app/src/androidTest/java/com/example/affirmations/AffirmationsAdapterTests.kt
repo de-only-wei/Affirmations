88bbccaa4287b4b988917f67eb0216b66f0ef83d
Zheng Wei <65906926+de-only-wei@users.noreply.github.com>
2023-02-07 10:55:13 -0800

    added some testing dependencies and unit, instrumentation tests

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,18 @@
+package com.example.affirmations
+
+import android.content.Context
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.model.Affirmation
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+
+class AffirmationsAdapterTests {
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size(){
+        
+    }
+}
